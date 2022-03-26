90a1b8a141f006b6ea30174934781651dad9172b
m7mdso3dy <kimosody125@gmail.com>
2022-03-26 22:15:03 +0200

    third project

--- reviews/setup/src/App.js
+++ reviews/setup/src/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import Review from './Review';
+import reviews from './data'
 function App() {
-  return <h2>reviews project setup</h2>;
+  // const [reviews, setReviews] = useState(data);
+  const [currentReview, setCurrentReview] = useState(0);
+  const chossingReviewHandler = (move) => {
+    if (move === 'next') {
+      setCurrentReview(old => {
+        old === reviews.length-1 ? old = 0 : old++; 
+        return old;
+      });
+    } else if (move === 'prev') {
+      setCurrentReview(old => {
+        old === 0 ? old = reviews.length-1 : old--; 
+        return old;
+      });
+    } else {
+      setCurrentReview(Math.floor(Math.random()* reviews.length))
+    }
+  }
+  return (
+    <Fragment>
+      <main>
+        <section className='container'>
+          <div className="title">
+            <h2>our reviews</h2>
+            <div className="unnder-line">
+            </div>
+          </div>
+          <Review
+            chossingReviewHandler = {chossingReviewHandler}
+            review = {reviews[currentReview]}
+          />
+        </section>
+      </main>
+    </Fragment>
+      
+      
+    )
 }
 
 export default App;
--- reviews/setup/src/Review.js
+++ reviews/setup/src/Review.js
@@ -1,9 +1,39 @@
-import React, { useState } from 'react';
-import people from './data';
+import React from 'react';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
-const Review = () => {
-  return <h2>review component</h2>;
+const Review = (props) => {
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={props.review.image} alt={props.review.name} className="person-img" />
+        <span className='quote-icon'>
+          <FaQuoteRight/>
+        </span>
+      </div>
+      <h4 className="author">{props.review.name}</h4>
+      <p className="job">{props.review.job}</p>
+      <p className="info"> {props.review.text} </p>
+      <div className="button-container">
+        <button onClick={() => {
+          props.chossingReviewHandler('prev')
+        }} className="prev-btn">
+          <FaChevronLeft/>
+        </button>
+        <button onClick={() => {
+          props.chossingReviewHandler('next')
+        }} className="next-btn">
+          <FaChevronRight/>
+        </button>
+      </div>
+      <button
+        onClick={() => {
+          props.chossingReviewHandler('rand')
+        }}
+        className="random-btn">
+          surprise me
+        </button>
+      </article>
+    )
 };
 
 export default Review;
